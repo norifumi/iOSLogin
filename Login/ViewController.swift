8649f54a6759726bd5a9bf8de28483e014234ba3
Norifumi Homma <norifumi.homma@gmail.com>
2020-05-06 19:29:54 +0900

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by NORIFUMI HOMMA on 2020/05/06.
+//  Copyright © 2020 init6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
