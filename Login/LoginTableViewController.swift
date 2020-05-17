20a553592ff482d1636e3a16a0acb2257dbade59
Norifumi Homma <norifumi.homma@gmail.com>
2020-05-17 16:30:03 +0900

    ログイン用 API クライアント実装

--- Login/LoginTableViewController.swift
+++ Login/LoginTableViewController.swift
@@ -9,33 +9,53 @@
 import UIKit
 
 class LoginTableViewController: UITableViewController {
-
+    
+    var sessionCreate: SessionController = SessionController(user: User())
+    var sessionAPIObserver: NSObjectProtocol?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         var user = User()
         user.email = "taro.appium@example.com"
-        user.password = "p@ssw0rd"
-        let sessionCreate: SessionCreate = SessionCreate(postParams: user.postParams)
-        sessionCreate.post()
+        user.password = "p@ssw0rdo"
+        sessionCreate = SessionController(user: user)
+        
+        sessionAPIObserver = NotificationCenter.default.addObserver(forName: .sessionAPIComplete, object: nil, queue: nil, using: {
+            (notification) in
+            print("APIリクエスト完了")
+            if notification.userInfo != nil {
+                if let userInfo = notification.userInfo as? [String: String?] {
+                    if userInfo["error"] != nil {
+                        print(userInfo["error"]!!)
+                        let alertView = UIAlertController(title: "エラー", message: "ログインに失敗しました", preferredStyle: .alert)
+                        alertView.addAction(UIAlertAction(title: "OK", style: .default) {
+                            action in return
+                        })
+                        self.present(alertView, animated: true, completion: nil)
+                    }
+                }
+            }
+        })
+        
+        sessionCreate.create()
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         switch section {
@@ -45,12 +65,12 @@ class LoginTableViewController: UITableViewController {
             return 0
         }
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let email_cell = tableView.dequeueReusableCell(withIdentifier: "Email") as! EmailTableViewCell
         let password_cell = tableView.dequeueReusableCell(withIdentifier: "Password") as! PasswordTableViewCell
-
+        
         // Configure the cell...
         if indexPath.section == 0 {
             if indexPath.row == 0 {
@@ -62,50 +82,50 @@ class LoginTableViewController: UITableViewController {
         return UITableViewCell()
     }
     
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
---
+++ Login/SessionController.swift
@@ -0,0 +1,94 @@
+//
+//  SessionCreate.swift
+//  Login
+//
+//  Created by NORIFUMI HOMMA on 2020/05/17.
+//  Copyright © 2020 init6. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+public extension Notification.Name {
+    public static let sessionAPIStart = Notification.Name("SessionAPIStart")
+    public static let sessionAPIComplete = Notification.Name("SessionAPIComplete")
+}
+
+public struct User: CustomStringConvertible {
+    public var id: Int = 0
+    public var name: String? = nil
+    public var email: String? = nil
+    public var password: String? = nil
+    public var access_token: String = ""
+    
+    public var description: String {
+        get {
+            var string = "\nName: \(String(describing: name))\n"
+            string += "\nEmail: \(String(describing: email))\n"
+            string += "\nPassword: \(String(describing: password))\n"
+            return string
+        }
+    }
+    
+    public var postParams: [String: String?] {
+        return ["user[email]": email,
+                "user[password]": password]
+    }
+    
+    public var idAccessToken: String {
+        return "\(id):\(access_token)"
+    }
+    
+    public mutating func parseIdAccessToken(idAccessToken: JSON) {
+        let id_access_token = idAccessToken["id_access_token"].string?.components(separatedBy: ":")
+        if id_access_token != nil && id_access_token?.count == 2 {
+            self.id = Int((id_access_token?[0])!)!
+            self.access_token = id_access_token?[1] as! String
+        }
+    }
+}
+
+public class SessionController {
+    let url = "http://192.168.226.150:3000/sessions.json"
+    var postParams: [String: String?]
+    var user: User = User()
+    
+    public init(user: User){
+        self.user = user
+        self.postParams = self.user.postParams
+    }
+    
+    public func create() {
+        NotificationCenter.default.post(name: .sessionAPIStart, object: nil)
+        
+        let request = AF.request(url, method: .post, parameters: postParams).response {
+            response in
+            var json = JSON.null
+            do {
+                if response.error == nil && response.data != nil {
+                    
+                    json = try SwiftyJSON.JSON(data: response.data!)
+                    self.user.parseIdAccessToken(idAccessToken: json)
+                }
+            } catch {
+                if let error = response.error {
+                    self.notifyError(errorMessage: "\(error)")
+                }
+                return
+            }
+            
+            self.notifyError(errorMessage: "メールアドレスまたはパスワードが間違えています")
+            
+            
+        }
+    }
+    
+    private func notifyError(errorMessage: String?){
+        var message = "Unknown error."
+        if errorMessage != nil {
+            message = errorMessage!
+        }
+        NotificationCenter.default.post(name: .sessionAPIComplete, object: nil, userInfo: ["error": message])
+    }
+}
